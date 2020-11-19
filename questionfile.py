bc66f94f906640c8e2b984258d0b0ed663799acc
Ceylan Şentürk <73878532+ceysenturk@users.noreply.github.com>
2020-11-20 00:38:43 +0300

    Delete questionfile.py

--- questionfile.py
+++
@@ -1,9 +0,0 @@
-def questionfunction():
-    questionfile = open("questionList.txt", encoding='utf8')
-    questionList = []
-    for line in questionfile:
-        stripped_line = line.strip()
-        line_list = stripped_line.split()
-        questionList.append(line_list)
-    questionfile.close()
-    return questionList
